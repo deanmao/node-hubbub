d017512dfb654273fe38cd2eaf90aac106cdb146
Dean Mao <deanmao@gmail.com>
2012-10-24 11:22:23 -0700

    clean up test

--- test.js
+++ test.js
@@ -13,33 +13,13 @@ if (binding === null) {
 }
 
 var z = new binding.Tokeniser();
-var fs = require('fs');
-var data = fs.readFileSync('mytest.html').toString();
 
-// z.process(data, function(err, res) {
-//   console.log(res);
-// });
-// z.process(data, function(err, res) {
-//   console.log(res);
-// });
-// z.process("<html>", function(err, res) {
-//   console.log(res);
-// });
-// z.process("<html>", function(err, res) {
-//   console.log(res);
-// });
-// z.process("<html>", function(err, res) {
-//   console.log(res);
-// });
-// z.process("<script>for(var i=0;i<n;i++);</script>", function(err, res) {
-//   console.log(res);
-// });
-// z.process("<html>", function(err, res) {
-//   console.log(res);
-// });
-
-fs.readFile('google.html', function(err, data) {
-  z.process(data.toString(), function(err, res) {
-    console.log(res);
-  });
-})
+z.process("<html>", function(err, res) {
+  console.log(res);
+});
+z.process("<html>", function(err, res) {
+  console.log(res);
+});
+z.process("<html>", function(err, res) {
+  console.log(res);
+});
