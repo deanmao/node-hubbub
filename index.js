707002be11586da87538586695af561624287450
Dean Mao <deanmao@gmail.com>
2012-10-28 02:01:21 -0700

    add a bunch of failing tests from node-htmlparser

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ function Parser(handler) {
   this.async = true;
 }
 
+Parser.prototype.reset = function() {
+    this.handler.reset();
+};
+
+Parser.prototype.done = function() {
+    this.handler.done();
+};
+
+
 Parser.prototype.parseComplete = function(chunk, cb, blocking) {
   if (this.handler.writeTag || blocking) {
     this.parseChunk(chunk, cb, true);
@@ -136,6 +145,296 @@ var ElementType = {
   , Style: "style" //Special tag <style>...</style>
   , Tag: "tag" //Any tag that isn't special
 }
+
+var Mode = {
+    Text: 'text',
+    Tag: 'tag',
+    Attr: 'attr',
+    CData: 'cdata',
+    Comment: 'comment'
+};
+
+
+
+function HtmlBuilder (callback, options) {
+    this.reset();
+    this._options = options ? options : { };
+    if (this._options.ignoreWhitespace === undefined) {
+        this._options.ignoreWhitespace = false; //Keep whitespace-only text nodes
+    }
+    if (this._options.includeLocation === undefined) {
+        this._options.includeLocation = false; //Include position of element (row, col) on nodes
+    }
+    if (this._options.verbose === undefined) {
+        this._options.verbose = true; //Keep data property for tags and raw property for all
+    }
+    if (this._options.enforceEmptyTags === undefined) {
+        this._options.enforceEmptyTags = true; //Don't allow children for HTML tags defined as empty in spec
+    }
+    if (this._options.caseSensitiveTags === undefined) {
+        this._options.caseSensitiveTags = false; //Lowercase all tag names
+    }
+    if (this._options.caseSensitiveAttr === undefined) {
+        this._options.caseSensitiveAttr = false; //Lowercase all attribute names
+    }
+    if ((typeof callback) == "function") {
+        this._callback = callback;
+    }
+}
+
+    //**"Static"**//
+    //HTML Tags that shouldn't contain child nodes
+    HtmlBuilder._emptyTags = {
+          area: 1
+        , base: 1
+        , basefont: 1
+        , br: 1
+        , col: 1
+        , frame: 1
+        , hr: 1
+        , img: 1
+        , input: 1
+        , isindex: 1
+        , link: 1
+        , meta: 1
+        , param: 1
+        , embed: 1
+        , '?xml': 1
+    };
+    //Regex to detect whitespace only text nodes
+    HtmlBuilder.reWhitespace = /^\s*$/;
+
+    //**Public**//
+    //Properties//
+    HtmlBuilder.prototype.dom = null; //The hierarchical object containing the parsed HTML
+    //Methods//
+    //Resets the builder back to starting state
+    HtmlBuilder.prototype.reset = function HtmlBuilder$reset() {
+        this.dom = [];
+        // this._raw = [];
+        this._done = false;
+        this._tagStack = [];
+        this._lastTag = null;
+        this._tagStack.last = function HtmlBuilder$_tagStack$last () {
+            return(this.length ? this[this.length - 1] : null);
+        };
+        this._line = 1;
+        this._col = 1;
+    };
+    //Signals the builder that parsing is done
+    HtmlBuilder.prototype.done = function HtmlBuilder$done () {
+        this._done = true;
+        this.handleCallback(null);
+    };
+
+    HtmlBuilder.prototype.error = function HtmlBuilder$error (error) {
+        this.handleCallback(error);
+    };
+
+    HtmlBuilder.prototype.handleCallback = function HtmlBuilder$handleCallback (error) {
+            if ((typeof this._callback) != "function") {
+                if (error) {
+                    throw error;
+                } else {
+                    return;
+                }
+            }
+            this._callback(error, this.dom);
+    };
+
+    HtmlBuilder.prototype.isEmptyTag = function HtmlBuilder$isEmptyTag (element) {
+        var name = element.name.toLowerCase();
+        if (name.charAt(0) == '?') {
+            return true;
+        }
+        if (name.charAt(0) == '/') {
+            name = name.substring(1);
+        }
+        return this._options.enforceEmptyTags && !!HtmlBuilder._emptyTags[name];
+    };
+
+    HtmlBuilder.prototype._getLocation = function HtmlBuilder$_getLocation () {
+        return { line: this._line, col: this._col };
+    };
+
+    // HtmlBuilder.reLineSplit = /(\r\n|\r|\n)/g;
+    HtmlBuilder.prototype._updateLocation = function HtmlBuilder$_updateLocation (node) {
+        var positionData = (node.type === Mode.Tag) ? node.raw : node.data;
+        if (positionData === null) {
+            return;
+        }
+        // var lines = positionData.split(HtmlBuilder.reLineSplit);
+        var lines = positionData.split("\n");
+        this._line += lines.length - 1;
+        if (lines.length > 1) {
+            this._col = 1;
+        }
+        this._col += lines[lines.length - 1].length;
+        if (node.type === Mode.Tag) {
+            this._col += 2;
+        } else if (node.type === Mode.Comment) {
+            this._col += 7;
+        } else if (node.type === Mode.CData) {
+            this._col += 12;
+        }
+    };
+
+    HtmlBuilder.prototype._copyElement = function HtmlBuilder$_copyElement (element) {
+        var newElement = { type: element.type };
+
+        if (this._options.verbose && element['raw'] !== undefined) {
+            newElement.raw = element.raw;
+        }
+        if (element['name'] !== undefined) {
+            switch (element.type) {
+
+                case Mode.Tag:
+                    newElement.name = this._options.caseSensitiveTags ?
+                        element.name
+                        :
+                        element.name.toLowerCase()
+                        ;
+                    break;
+
+                case Mode.Attr:
+                    newElement.name = this._options.caseSensitiveAttr ?
+                        element.name
+                        :
+                        element.name.toLowerCase()
+                        ;
+                    break;
+
+                default:
+                    newElement.name = this._options.caseSensitiveTags ?
+                        element.name
+                        :
+                        element.name.toLowerCase()
+                        ;
+                    break;
+
+            }
+        }
+        if (element['data'] !== undefined) {
+            newElement.data = element.data;
+        }
+        if (element.location) {
+            newElement.location = { line: element.location.line, col: element.location.col };
+        }
+
+        return newElement;
+    };
+
+    HtmlBuilder.prototype.write = function HtmlBuilder$write (element) {
+        // this._raw.push(element);
+        if (this._done) {
+            this.handleCallback(new Error("Writing to the builder after done() called is not allowed without a reset()"));
+        }
+        if (this._options.includeLocation) {
+            if (element.type !== Mode.Attr) {
+                element.location = this._getLocation();
+                this._updateLocation(element);
+            }
+        }
+        if (element.type === Mode.Text && this._options.ignoreWhitespace) {
+            if (HtmlBuilder.reWhitespace.test(element.data)) {
+                return;
+            }
+        }
+        var parent;
+        var node;
+        if (!this._tagStack.last()) { //There are no parent elements
+            //If the element can be a container, add it to the tag stack and the top level list
+            if (element.type === Mode.Tag) {
+                if (element.name.charAt(0) != "/") { //Ignore closing tags that obviously don't have an opening tag
+                    node = this._copyElement(element);
+                    this.dom.push(node);
+                    if (!this.isEmptyTag(node)) { //Don't add tags to the tag stack that can't have children
+                        this._tagStack.push(node);
+                    }
+                    this._lastTag = node;
+                }
+            } else if (element.type === Mode.Attr && this._lastTag) {
+                if (!this._lastTag.attributes) {
+                    this._lastTag.attributes = {};
+                }
+                this._lastTag.attributes[this._options.caseSensitiveAttr ? element.name : element.name.toLowerCase()] =
+                    element.data;
+            } else { //Otherwise just add to the top level list
+                this.dom.push(this._copyElement(element));
+            }
+        } else { //There are parent elements
+            //If the element can be a container, add it as a child of the element
+            //on top of the tag stack and then add it to the tag stack
+            if (element.type === Mode.Tag) {
+                if (element.name.charAt(0) == "/") {
+                    //This is a closing tag, scan the tagStack to find the matching opening tag
+                    //and pop the stack up to the opening tag's parent
+                    var baseName = this._options.caseSensitiveTags ?
+                        element.name.substring(1)
+                        :
+                        element.name.substring(1).toLowerCase()
+                        ;
+                    if (!this.isEmptyTag(element)) {
+                        var pos = this._tagStack.length - 1;
+                        while (pos > -1 && this._tagStack[pos--].name != baseName) { }
+                        if (pos > -1 || this._tagStack[0].name == baseName) {
+                            while (pos < this._tagStack.length - 1) {
+                                this._tagStack.pop();
+                            }
+                        }
+                    }
+                }
+                else { //This is not a closing tag
+                    parent = this._tagStack.last();
+                    if (element.type === Mode.Attr) {
+                        if (!parent.attributes) {
+                            parent.attributes = {};
+                        }
+                        parent.attributes[this._options.caseSensitiveAttr ? element.name : element.name.toLowerCase()] =
+                            element.data;
+                    } else {
+                        node = this._copyElement(element);
+                        if (!parent.children) {
+                            parent.children = [];
+                        }
+                        parent.children.push(node);
+                        if (!this.isEmptyTag(node)) { //Don't add tags to the tag stack that can't have children
+                            this._tagStack.push(node);
+                        }
+                        if (element.type === Mode.Tag) {
+                            this._lastTag = node;
+                        }
+                    }
+                }
+            }
+            else { //This is not a container element
+                parent = this._tagStack.last();
+                if (element.type === Mode.Attr) {
+                    if (!parent.attributes) {
+                        parent.attributes = {};
+                    }
+                    parent.attributes[this._options.caseSensitiveAttr ? element.name : element.name.toLowerCase()] =
+                        element.data;
+                } else {
+                    if (!parent.children) {
+                        parent.children = [];
+                    }
+                    parent.children.push(this._copyElement(element));
+                }
+            }
+        }
+    };
+
+
+    //**Private**//
+    //Properties//
+    HtmlBuilder.prototype._options = null; //Builder options for how to behave
+    HtmlBuilder.prototype._callback = null; //Callback to respond to when parsing done
+    HtmlBuilder.prototype._done = false; //Flag indicating whether builder has been notified of parsing completed
+    HtmlBuilder.prototype._tagStack = null; //List of parents to the currently element being processed
+    //Methods//
+
+exports.HtmlBuilder = HtmlBuilder;
 
 // DefaultHandler from tautologistics/node-htmlparser 1.x
 function DefaultHandler (callback, options) {
---
+++ runtests.js
@@ -0,0 +1,224 @@
+/***********************************************
+Copyright 2010 - 2012, Chris Winberry <chris@winberry.net>. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+***********************************************/
+
+Object.prototype.equals = function (x) {
+    //http://stackoverflow.com/questions/1068834/object-comparison-in-javascript
+    var p;
+
+    for (p in this) {
+        if (typeof(x[p]) == 'undefined') {
+            // console.log('Missing property: ', p);
+            return false;
+        }
+    }
+
+    for (p in x) {
+        if (typeof(this[p]) == 'undefined') {
+            // console.log('Extra property: ', p);
+            return false;
+        }
+    }
+
+    for (p in this) {
+        if (this[p]) {
+            switch(typeof(this[p])) {
+                case 'object':
+                    if (!this[p].equals(x[p])) {
+                        // console.log('Mismatched property: ', p);
+                        return false;
+                    }
+                    break;
+                case 'function':
+                    if (typeof(x[p])=='undefined' || (p != 'equals' && this[p].toString() != x[p].toString())) {
+                        // console.log('Mismatched property: ', p);
+                        return false;
+                    }
+                    break;
+                default:
+                    if (this[p] != x[p]) {
+                        // console.log('Mismatched property: ', p);
+                        return false;
+                    }
+            }
+        } else {
+            if (x[p]) {
+                // console.log('Poop: ', p);
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+var util = require("util");
+var fs = require("fs");
+var htmlparser = require("./index");
+
+var testUtils = require('./tests/testutils');
+var htmlTests = require('./tests/html');
+// var rssTests = require('./tests/rss');
+var parserTests = require('./tests/parser');
+
+var testResults = {};
+
+testUtils.runBuilderTests(
+      htmlTests
+    , htmlparser.Parser
+    , htmlparser.HtmlBuilder
+    , null
+    , function (testName, testResult, actual, expected) {
+        console.log("[" + testName + "]: " + (testResult ? "passed" : "FAILED"));
+    }, function (elapsed, passed, failed) {
+        testResults['HTML builder'] = {
+              elapsed: elapsed
+            , passed: passed
+            , failed: failed
+            };
+    });
+testUtils.runBuilderTests(
+      htmlTests
+    , htmlparser.Parser
+    , htmlparser.HtmlBuilder
+    , function (test) {
+        var newTest = {};
+        for (var key in test) {
+            if (!test.hasOwnProperty(key)) {
+                continue;
+            }
+            newTest[key] = (key === 'data') ?
+                test.data.join('').split('')
+                :
+                test[key]
+                ;
+        }
+        return newTest;
+    }
+    , function (testName, testResult, actual, expected) {
+        console.log("[" + testName + "]: " + (testResult ? "passed" : "FAILED"));
+    }, function (elapsed, passed, failed) {
+        testResults['HTML builder (streamed)'] = {
+              elapsed: elapsed
+            , passed: passed
+            , failed: failed
+            };
+    });
+
+// testUtils.runBuilderTests(
+//       rssTests
+//     , htmlparser.Parser
+//     , htmlparser.RssBuilder
+//     , null
+//     , function (testName, testResult, actual, expected) {
+//         console.log("[" + testName + "]: " + (testResult ? "passed" : "FAILED"));
+//     }, function (elapsed, passed, failed) {
+//         testResults['RSS builder'] = {
+//               elapsed: elapsed
+//             , passed: passed
+//             , failed: failed
+//             };
+//     });
+// testUtils.runBuilderTests(
+//       rssTests
+//     , htmlparser.Parser
+//     , htmlparser.RssBuilder
+//     , function (test) {
+//         var newTest = {};
+//         for (var key in test) {
+//             if (!test.hasOwnProperty(key)) {
+//                 continue;
+//             }
+//             newTest[key] = (key === 'data') ?
+//                 test.data.join('').split('')
+//                 :
+//                 test[key]
+//                 ;
+//         }
+//         return newTest;
+//     }
+//     , function (testName, testResult, actual, expected) {
+//         console.log("[" + testName + "]: " + (testResult ? "passed" : "FAILED"));
+//     }, function (elapsed, passed, failed) {
+//         testResults['RSS builder (streamed)'] = {
+//               elapsed: elapsed
+//             , passed: passed
+//             , failed: failed
+//             };
+//     });
+
+testUtils.runParserTests(
+      parserTests
+    , htmlparser.Parser
+    , null
+    , function (testName, testResult, actual, expected) {
+        console.log("[" + testName + "]: " + (testResult ? "passed" : "FAILED"));
+    }, function (elapsed, passed, failed) {
+        testResults['Parser'] = {
+              elapsed: elapsed
+            , passed: passed
+            , failed: failed
+            };
+    });
+testUtils.runParserTests(
+      parserTests
+    , htmlparser.Parser
+    , function (test) {
+        var newTest = {};
+        for (var key in test) {
+            if (!test.hasOwnProperty(key)) {
+                continue;
+            }
+            newTest[key] = (key === 'data') ?
+                test.data.join('').split('')
+                :
+                test[key]
+                ;
+        }
+        return newTest;
+    }
+    , function (testName, testResult, actual, expected) {
+        console.log("[" + testName + "]: " + (testResult ? "passed" : "FAILED"));
+    }, function (elapsed, passed, failed) {
+        testResults['Parser (streamed)'] = {
+              elapsed: elapsed
+            , passed: passed
+            , failed: failed
+            };
+    });
+
+console.log('');
+console.log('Test Results');
+console.log('------------------');
+var passedTotal = 0;
+var failedTotal = 0;
+var elapsedTotal = 0;
+for (var testName in testResults) {
+    if (!testResults.hasOwnProperty(testName)) {
+        continue;
+    }
+    var test = testResults[testName];
+    passedTotal += test.passed;
+    failedTotal += test.failed;
+    elapsedTotal += test.elapsed;
+    console.log(testName + ': ' + test.passed + '/' + (test.passed + test.failed) + ' (' + Math.round(test.passed / (test.passed + test.failed) * 100) + '%) [' + test.elapsed + 'ms]');
+}
+console.log('------------------');
+console.log('Total: ' + passedTotal + '/' + (passedTotal + failedTotal) + ' (' + Math.round(passedTotal / (passedTotal + failedTotal) * 100) + '%) [' + elapsedTotal + 'ms]');
